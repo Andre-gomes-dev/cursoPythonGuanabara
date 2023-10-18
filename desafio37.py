f0e178587bb689d251ae833e4f3638f282f29977
André Gomes <83479122+Andre-gomes-dev@users.noreply.github.com>
2023-10-18 11:36:55 -0300

    precisa terminar

--- desafio37.py
+++ desafio37.py
@@ -1,14 +1,17 @@
 
 print("escolha uma das bases para conversão: ")
+opcao  = input("sua opção: ")
 numero = int(input("digite um numero inteiro: "))
 
 conversor = []
+divisaoResto = 0
+if opcao == 1: #binario
+  while numero >0: 
+        divisaoResto =  numero  % 2
+        conversor.append(divisaoResto)
+        numero =  numero  // 2
+        print(numero)              
+else:
+    print("escolha uma opção valida") 
 
-
-while numero >=0: 
-       # divisaoResto =  numero  % 2
-        # conversor.append(divisaoResto)
-        numero =  numero  / 2
-        print(numero)
-
-print(conversor)    
+print(conversor)   
