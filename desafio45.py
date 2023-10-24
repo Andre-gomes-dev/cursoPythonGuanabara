499501ca9c26a8668eb39f6807e978ecd3dff471
andre-gomes <gomesandre749@gmail.com>
2023-10-24 09:11:32 -0300

    teste de commit

--- desafio45.py
+++ desafio45.py
@@ -1,13 +1,21 @@
 import random
 
+#arrays
 jogadas = [0,1,2]
-jogada = int(input("qual é a sua jogada"))
+tipoJoken = ["PEDRA", "PAPEL", "TESOURA"]
 
+#interação com usuario
 print("[0] PEDRA")
 print("[1] PAPEL")
 print("[2] TESOURA")
+jogada = int(input("qual é a sua jogada"))
 
+#interação da maquina ao capturar um numero aleatorio
 jokenpo = random.randint(0, len(jogadas))
+juncaoJoken = [tipoJoken[i] for i in jogadas]
+
+print(juncaoJoken[jokenpo])
 print(jokenpo)
+
 
 
