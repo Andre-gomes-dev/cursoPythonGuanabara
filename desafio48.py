f189911e4ec9fa9168697518350592c5a952a033
andre-gomes <gomesandre749@gmail.com>
2023-10-26 23:55:27 -0300

    desafio 48 finalizado

--- desafio48.py
+++ desafio48.py
@@ -1,13 +1,10 @@
-#numero =  input("digite algum numero")
 impar = []
-i = 0
 soma = 0
 #primeiro achar o impar
-while i <= 500:
-        resultado = i /  3
-        if i % 2 == 1 and  resultado.is_integer:   
-                impar.append(i)
-                soma = soma + i
-        i+=1
-print(len(impar)) 
-print(soma)   
+
+for i in range(1, 501, 2):
+    if i % 3 == 0:
+        impar.append(i)
+        soma += i
+print("o tamanho dos numeros primos é: {} ".format(len(impar)))   
+print("a soma de todos esses numeros é: {} ".format(soma))      
