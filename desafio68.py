2c32f3d8f35e01e677ce4681c3b517113be82ad5
andre-gomes <gomesandre749@gmail.com>
2023-11-08 21:20:51 -0300

    ta funcionando mas aparenta esta com defeitos. melhorar depois

--- desafio68.py
+++ desafio68.py
@@ -9,17 +9,19 @@ print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 numero = int(input("diga um numero de 0 a 10:  "))
 escolha = input("PAR ou IMPAR? [P/I]: ").upper().strip()
 
-jogo = aleatorio + numero
+#variaveis
+jogo = int(aleatorio + numero)
 vitoria = ""
+jogador = ""
+computador = ""
+
+
+#logica de par ou impar
 if jogo % 2 == 0:
     vitoria = "PAR"
-    vencedor = 
 elif jogo %2 == 1:
-    vitoria = "IMPAR"
-
-print("o seu numero é: {} e o computador escolheu {}, logo deu {},  resultando em: {} ".format(numero, aleatorio, jogo, vitoria))
-
-    
+    vitoria = "IMPAR"    
+#atribuição referente a par e impar
 if escolha == "P":
      jogador = "PAR"
      computador = "IMPAR"
@@ -28,6 +30,18 @@ elif escolha == "I":
      computador = "PAR"
 else:
      print("digite uma opção válida")
+
+#logica do vencedor
+
+if vitoria in jogador:
+      print("voce venceu")
+elif vitoria in computador:
+      print("computador venceu")     
+
+print("o seu numero é: {} e o computador escolheu {}, logo deu {},  resultando em: {} ".format(numero, aleatorio, jogo, vitoria))
+
+    
+
 #logica de divisao para saber se é par ou impar
 
 
