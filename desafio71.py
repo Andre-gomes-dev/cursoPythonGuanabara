4d69caba2c7988d33ec813dd24666d2630264180
andre-gomes <gomesandre749@gmail.com>
2023-11-10 00:26:19 -0300

    incompleto

---
+++ desafio71.py
@@ -0,0 +1,13 @@
+from itertools import count
+from operator import index
+
+
+
+cedulas = [50, 10, 1]
+valor =input("digitar o valor retirado: ")
+teste = list(valor)
+tamanho = len(valor)
+
+#logica de divisao do valor com o tamanho da palavra:
+print(tamanho)
+print(teste)
