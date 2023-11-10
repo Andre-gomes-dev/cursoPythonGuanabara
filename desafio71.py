a13f2dcdbc704190eee8a38e714d8162cb2442d6
andre-gomes <gomesandre749@gmail.com>
2023-11-10 11:31:00 -0300

    ainda incompleto

--- desafio71.py
+++ desafio71.py
@@ -1,13 +1,24 @@
 from itertools import count
 from operator import index
-
-
+import math
+print("=-=-=-=-=-=-=-=--=-=-=-=-=-=--==-=-=--=-=-=-=-")
+print("                   BANCO CEV                ")
+print("=-=-=-=-=-=-=-=--=-=-=-=-=-=--==-=-=--=-=-=-=-")
 
 cedulas = [50, 10, 1]
-valor =input("digitar o valor retirado: ")
-teste = list(valor)
-tamanho = len(valor)
+valor =int(input("digitar o valor retirado: "))
+# teste = list(valor)
+# tamanho = len(valor)
 
 #logica de divisao do valor com o tamanho da palavra:
-print(tamanho)
-print(teste)
+
+calculo50 = valor // 50
+total = calculo50 * 50
+
+valor10 = valor  - total 
+calcula10 =  valor10 // 10
+
+valor5 =  valor
+print(calculo50, calcula10, valor10)
+
+# print(tamanho)
